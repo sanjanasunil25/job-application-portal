2d3208dbc486ab1fa855f5501db8d8671fa25682
sanjanasunil25 <sanjanagollappalli@gmail.com>
2026-02-23 18:43:56 +0530

    Final submission version

--- frontend/app/jobs/create/page.tsx
+++ frontend/app/jobs/create/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import Navbar from "../../../components/Navbar";
+import Navbar from "../../components/Navbar";
 
 export default function CreateJobPage() {
   const router = useRouter();
